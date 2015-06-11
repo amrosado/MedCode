3fe8095a99b58a72e63ee359d4b7da20d5365489
Aaron Rosado <aaron.rosado@gmail.com>
2015-06-11 18:45:24 -0400

    Parsed a majority of the wiki article information.  Still need to work on expanding the information retrieved from the wiki page.  Also work on the natural language processing component.

--- WIkiMedIcdInfoExtend.py
+++ WIkiMedIcdInfoExtend.py
@@ -16,6 +16,7 @@ class WikiMedIcdInfoExtend:
     wikiMedInfoDatabase = None
 
     wikiSearchUrl = 'https://en.wikipedia.org?search='
+    wikiBaseUrl = 'https://en.wikipedia.org/'
 
     def constructWikiInformationExtension(self):
         try:
@@ -41,15 +42,63 @@ class WikiMedIcdInfoExtend:
         wikiInfo = htmlSoup.find('table', id='infobox')
         contentText = htmlSoup.find('div', id='mw-content-text')
         pageTitle = htmlSoup.find('h1', id='firstHeading')
-
+        data = {}
+        sectionData = {}
+        sectionTextHolder = []
+        sectionLinkHolder = []
+        imageLinkHolder = []
+        sectionDataHolder = []
+        referenceHolder = []
         if contentText != None:
             mainContentChildren = contentText.children
-            for contentChildren in mainContentChildren:
-                if type(contentChildren) is Tag:
-                    if contentChildren.name == 'div':
-                        pass
-                    if contentChildren.name == 'p':
-                        pass
+            data['title'] = pageTitle.text
+            sectionData['name'] = 'summary'
+            for contentChild in mainContentChildren:
+                if type(contentChild) is Tag:
+                    if contentChild.name == 'h2':
+                        sectionData['text'] = sectionTextHolder
+                        sectionData['links'] = sectionLinkHolder
+                        if len(sectionLinkHolder) > 0:
+                            sectionData['links'] = sectionLinkHolder
+                        if len(imageLinkHolder) > 0:
+                            sectionData['imageLinks'] = imageLinkHolder
+                        sectionDataHolder.append(sectionData)
+                        sectionData = {}
+                        sectionLinkHolder = []
+                        imageLinkHolder = []
+                        sectionTextHolder = []
+                        sectionData['sectionName'] = contentChild.text
+                    elif contentChild.name == 'div':
+                        imageLinks = contentChild.find_all('a', 'image')
+                        articleLinks = contentChild.find_all('a', 'hatnote')
+                        referenceSpans = contentChild.find_all('span', 'reference-text')
+                        if referenceSpans != None:
+                            for referenceSpan in referenceSpans:
+                                referenceLinks = referenceSpan.find_all('a')
+                                if referenceLinks != None:
+                                    referenceText = referenceSpan.get_text()
+                                    referenceLinkHolder = []
+                                    for referenceLink in referenceLinks:
+                                        referenceLinkHolder.append([referenceText, referenceLink.text, referenceLink['href']])
+                                    referenceHolder.append({'text': referenceText, 'links': referenceLinkHolder})
+                                else:
+                                    referenceHolder.append([referenceText])
+                        if imageLinks != None:
+                            for imageLink in imageLinks:
+                                imageLinkHolder.append(imageLink['href'])
+                        if articleLinks != None:
+                            for articleLink in articleLinks:
+                                sectionLinkHolder.append([articleLink['title'], articleLink['href']])
+                    elif contentChild.name == 'p':
+                        contentLinks = contentChild.find_all('a')
+                        if contentLinks != None:
+                            for link in contentLinks:
+                                sectionLinkHolder.append([link.text, link['href']])
+                        sectionTextHolder.append(contentChild.get_text())
+            data['sectionData'] = sectionDataHolder
+            data['referenceInfo'] = referenceHolder
+            pass
+
 
 
     def processWikiSearchHtml(self, htmlSoup):
